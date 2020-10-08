91a034069e1e89048ca9c9ec173d43e4635eb66a
zyz <1479787237@qq.com>
2020-10-08 21:35:09 +0800

    项目web化并测试

---
+++ README.md
@@ -0,0 +1,2 @@
+# SSMShoping
+以SSM框架搭建的分布式商城系统
--- e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,14 +1,13 @@
 package cn.e3mall.service.impl;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.e3mall.mapper.ItemMapper;
 import cn.e3mall.pojo.Item;
-import cn.e3mall.pojo.ItemExample;
-import cn.e3mall.pojo.ItemExample.Criteria;
+
 import cn.e3mall.service.ItemService;
 /**
  * 商品管理Service
