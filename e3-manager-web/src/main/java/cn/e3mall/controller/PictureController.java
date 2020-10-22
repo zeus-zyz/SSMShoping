16a438403374124a4c02750bd62c7569a88726db
zyz <1479787237@qq.com>
2020-10-22 20:25:18 +0800

    添加商品信息和上传图片并测试

--- e3-manager-web/src/main/java/cn/e3mall/controller/PictureController.java
+++ e3-manager-web/src/main/java/cn/e3mall/controller/PictureController.java
@@ -32,11 +32,16 @@ public class PictureController {
 	@ResponseBody
 	public String uplodFile(MultipartFile uploadFile){
 		try{
+			//把图片上传到图片服务器
 			FastDFSClient fastDFSClient = new FastDFSClient("classpath:conf/client.conf");
+			//取文件的扩展名
 			String originalFilename = uploadFile.getOriginalFilename();
 			String extName = originalFilename.substring(originalFilename.lastIndexOf(".")+1);
+			//得到一个图片的地址和文件名
 			String url = fastDFSClient.uploadFile(uploadFile.getBytes(),extName);
+			//补全完整的URL
 			url=IMAGE_SERVER_URL + url;
+			//封装到Map集合中并返回
 			Map result=new HashMap<>();
 			result.put("error", 0);
 			result.put("url", url);
--- e3-manager-web/src/test/java/cn/e3mall/test/fast/FastDfsTest.java
+++ e3-manager-web/src/test/java/cn/e3mall/test/fast/FastDfsTest.java
@@ -8,6 +8,8 @@ import org.csource.fastdfs.TrackerClient;
 import org.csource.fastdfs.TrackerServer;
 import org.junit.Test;
 
+import cn.e3mall.common.untils.FastDFSClient;
+
 /**
  * @Description:
  * @version 1.0
@@ -20,22 +22,30 @@ import org.junit.Test;
 public class FastDfsTest {
 
 	@Test
-	public void testUpload() throws Exception{
-		//创建一个配置文件 其中有tracker服务器的地址
-		//使用全局对象加载配置文件
-		ClientGlobal.init("E:/rope/SSMShoping/e3-manager-web/src/main/resources/conf/client.conf");
-		//创建一个trackerClient对象
+	public void testUpload() throws Exception {
+		//创建一个配置文件。文件名任意。内容就是tracker服务器的地址。
+		//使用全局对象加载配置文件。
+		ClientGlobal.init("E:\\rope\\SSMShoping\\e3-manager-web\\src\\main\\resources\\conf\\client.conf");
+		//创建一个TrackerClient对象
 		TrackerClient trackerClient = new TrackerClient();
-		//通过trackerClient获得一个trackerServer对象
+		//通过TrackClient获得一个TrackerServer对象
 		TrackerServer trackerServer = trackerClient.getConnection();
-		//创建一个storageServer的引用，可以为null
-		StorageServer storageServer=null;
-		//创建一个storageClient，参数需要trackerServer和storageServer
-		StorageClient storageClient = new StorageClient(trackerServer,storageServer);
-		//使用storageClient上传文件
-		String[] strings = storageClient.upload_file("F:/Desktop/1.png", "png", null);
-		for (String string : strings) {
+		//创建一个StrorageServer的引用，可以是null
+		StorageServer storageServer = null;
+		//创建一个StorageClient，参数需要TrackerServer和StrorageServer
+		StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+		//使用StorageClient上传文件。
+		String[] upload_file = storageClient.upload_file("D:/images/log/9a64fce785.jpg", "jpg", null);
+		for (String string : upload_file) {
 			System.out.println(string);
 		}
+	}
+	
+	
+	@Test
+	public void testFastDFSClient() throws Exception{
+		FastDFSClient fastDFSClient = new FastDFSClient("E:\\rope\\SSMShoping\\e3-manager-web\\src\\main\\resources\\conf\\client.conf");
+		String string = fastDFSClient.uploadFile("F:\\1.png");
+		System.out.println(string);
 	}
 }
